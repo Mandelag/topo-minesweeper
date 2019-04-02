5d704118b4ec3106066aadf0515be84bee04fdfa
Keenan Gebze <keenan@bahasa.ai>
2019-04-02 23:57:26 +0700

    Add new unit test.

--- src/test/java/minesweeper/UtilsTest.java
+++ src/test/java/minesweeper/UtilsTest.java
@@ -57,4 +57,30 @@ public class UtilsTest {
         new int[]{3},
     }, connectivityMatrix);
   }
+
+  /**
+   * We also have to handle arc indexes with negative value according to the TopoJSON spec article 2.1.4:
+   * https://github.com/topojson/topojson-specification
+   */
+  @Test
+  public void getConnectivityMatrixWithNegativeIndexesTest() {
+    List<IPolygon> polygons = Arrays.asList(new IPolygon[]{
+        new Polygon(0, new int[][]{ new int[]{0, 1, -3}}),  // A
+        new Polygon(1, new int[][]{ new int[]{0}}),  // B
+        new Polygon(2, new int[][]{ new int[]{2, -4}}),  // C
+        new Polygon(3, new int[][]{ new int[]{1, -5, 5}}),     // D
+        new Polygon(4, new int[][]{ new int[]{3, 4}}),    // E
+        new Polygon(5, new int[][]{ new int[]{5}}) // F
+    });
+
+    int[][] connectivityMatrix = Utils.getConnectivityMatrix(polygons);
+    assertArrayEquals(new int[][] {
+        new int[]{1, 2, 3},
+        new int[]{0},
+        new int[]{0, 4},
+        new int[]{0, 4, 5},
+        new int[]{2, 3},
+        new int[]{3},
+    }, connectivityMatrix);
+  }
 }
