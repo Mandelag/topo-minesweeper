e7e77bff6e236ef336e30911bb0cf39437f64247
Keenan Gebze <keenan@bahasa.ai>
2019-04-02 14:49:16 +0700

    half finished the polygonsToGraph utilities.

--- src/main/java/minesweeper/Utils.java
+++ src/main/java/minesweeper/Utils.java
@@ -7,25 +7,18 @@ import java.util.Map;
 import topojson.IPolygon;
 
 public class Utils {
-  public static void polygonsToGraph(List<IPolygon>) {
+  public static void polygonsToGraph(List<IPolygon> polygons) {
     Map<String, Integer> externalToInternalIdMapping = new HashMap<>(polygons.size()); // differentiate between Id in the input poylgons and id represented in graph
-    Map<Integer, Map<String, Object>> arcs = new HashMap<>(); // mapping between a boundary and their polygons
+    Map<Integer, List<IPolygon>> arcs = new HashMap<>(); // mapping between a boundary and their polygons
 
-    for ( Map<String, Object> polygon : polygons ) {
-      String externalId = String.valueOf(polygon.get("id"));
-      List<List<Integer>> polygonRings = (List<List<Integer>>) polygon.get("arcs");
-      List<Integer> outerRingArcIndexes = polygonRings.get(0);
-      if (externalId == null) {
-        throw new NullPointerException("Polygon objects must have a valid \"id\" attribute.");
-      }
-      if(outerRingArcIndexes == null){
-        throw new NullPointerException("Polygon object must have a valid \"arcs\" attribute.");
-      }
-
-      for( Integer boundaryIndex : outerRingArcIndexes) {
-        List<Object> neighboringPolygons = arcs.getOrDefault(boundaryIndex, new ArrayList<Object>());
-        neighboringPolygons.add(polygons);
+    for (IPolygon polygon : polygons) {
+      int[] outerRingArcIndexes = polygon.getArcs()[0];
+      for(int i=0; i<outerRingArcIndexes.length; i++) {
+        List<IPolygon> neighboringPolygons = arcs.getOrDefault(outerRingArcIndexes[i], new ArrayList<>());
+        neighboringPolygons.add(polygon);
       }
     }
+
+
   }
 }
