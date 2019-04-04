f6d59304758b5cd02b76a4402d072a95c9b24487
Keenan Gebze <keenan@bahasa.ai>
2019-04-04 23:15:01 +0700

    Remove redundant 2d declarations again..

--- src/test/java/minesweeper/UtilsTest.java
+++ src/test/java/minesweeper/UtilsTest.java
@@ -67,23 +67,23 @@ public class UtilsTest {
   @Test
   public void getConnectivityMatrixWithNegativeIndexesTest() {
     List<IPolygon> polygons = Arrays.asList(new IPolygon[]{
-        new Polygon(0, new int[][]{ new int[]{0, 1, -3}}),  // A
-        new Polygon(1, new int[][]{ new int[]{0}}),  // B
-        new Polygon(2, new int[][]{ new int[]{2, -4}}),  // C
-        new Polygon(3, new int[][]{ new int[]{1, -5, 5}}),     // D
-        new Polygon(4, new int[][]{ new int[]{3, 4}}),    // E
-        new Polygon(5, new int[][]{ new int[]{5}}) // F
+        new Polygon(0, new int[][]{{0, 1, -3}}),  // A
+        new Polygon(1, new int[][]{{0}}),  // B
+        new Polygon(2, new int[][]{{2, -4}}),  // C
+        new Polygon(3, new int[][]{{1, -5, 5}}),     // D
+        new Polygon(4, new int[][]{{3, 4}}),    // E
+        new Polygon(5, new int[][]{{5}}) // F
     });
 
     int[][] connectivityMatrix = Utils.getConnectivityMatrix(polygons);
 
     assertArrayEquals(new int[][] {
-        new int[]{1, 2, 3},
-        new int[]{0},
-        new int[]{0, 4},
-        new int[]{0, 4, 5},
-        new int[]{2, 3},
-        new int[]{3},
+        {1, 2, 3},
+        {0},
+        {0, 4},
+        {0, 4, 5},
+        {2, 3},
+        {3},
     }, connectivityMatrix);
   }
 }
